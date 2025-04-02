e4451fe1222c7a86899f959707c64b7c410a917d
shreyaskamble1612 <shreyaskamble393@gmail.com>
2025-04-03 00:32:30 +0530

    AccChart

--- expensync/app/(main)/dashboard/page.jsx
+++ expensync/app/(main)/dashboard/page.jsx
@@ -1,13 +1,12 @@
 import { Suspense } from "react";
 import { getUserAccounts } from "@/actions/dashboard";
 import { getDashboardData } from "@/actions/dashboard";
-import { getCurrentBudget } from "@/actions/budget";
-import { AccountCard } from "./_components/account-card";
 import { CreateAccountDrawer } from "@/components/create-account-drawer";
-import { BudgetProgress } from "./_components/budget-progress";
 import { Card, CardContent } from "@/components/ui/card";
+import { BudgetProgress } from "./_components/budget-progress";
+
 import { Plus } from "lucide-react";
-import { DashboardOverview } from "./_components/transaction-overview";
+
 
 export default async function DashboardPage() {
   const [accounts, transactions] = await Promise.all([
